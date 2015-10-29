9df4ee194d3905572acea8aff9dc8f46d55e916f
Angel <angel@mozillafoundation.org>
2015-10-29 15:41:45 -0700

    underscore removed

--- prototypes/static-admin-prototype/dist/js/pages/interactions.js
+++ prototypes/static-admin-prototype/dist/js/pages/interactions.js
@@ -1,22 +1,8 @@
 $(".content").load("_dashboard.html");
-
+$(".knob").knob();
 $(function () {
-    $(".knob").knob();
     
-           function clock() {
-            var $s = $(".second"),
-                $m = $(".minute"),
-                $h = $(".hour");
-                d = new Date(),
-                s = d.getSeconds(),
-                m = d.getMinutes(),
-                h = d.getHours();
-            $s.val(s).trigger("change");
-            $m.val(m).trigger("change");
-            $h.val(h).trigger("change");
-            setTimeout("clock()", 1000);
-        }
-        clock(); 
+     
     
   'use strict';
  /* jVector Maps
