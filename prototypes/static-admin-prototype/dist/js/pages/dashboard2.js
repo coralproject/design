0a83d1f431f6d2588b2a46a2092772f00c801696
Angel <angel@mozillafoundation.org>
2015-10-29 15:11:47 -0700

    adding jQuery Knob

--- prototypes/static-admin-prototype/dist/js/pages/dashboard2.js
+++ prototypes/static-admin-prototype/dist/js/pages/dashboard2.js
@@ -2,6 +2,7 @@ $(function () {
 
   'use strict';
 
+
   /* ChartJS
    * -------
    * Here we will create a few charts using ChartJS
--- prototypes/static-admin-prototype/dist/js/pages/interactions.js
+++ prototypes/static-admin-prototype/dist/js/pages/interactions.js
@@ -1,7 +1,23 @@
 $(".content").load("_dashboard.html");
 
 $(function () {
-
+    $(".knob").knob();
+    
+           function clock() {
+            var $s = $(".second"),
+                $m = $(".minute"),
+                $h = $(".hour");
+                d = new Date(),
+                s = d.getSeconds(),
+                m = d.getMinutes(),
+                h = d.getHours();
+            $s.val(s).trigger("change");
+            $m.val(m).trigger("change");
+            $h.val(h).trigger("change");
+            setTimeout("clock()", 1000);
+        }
+        clock(); 
+    
   'use strict';
  /* jVector Maps
    * ------------
